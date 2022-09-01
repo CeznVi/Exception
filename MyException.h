a9616fd02a07d092944a9754c956f01970d52864
Viktor <Cezn.Vi@gmail.com>
2022-09-01 21:11:44 +0300

    start

---
+++ MyException.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include<fstream>
+
+class MyException : public exception
+{
+	string err;
+
+public:
+	MyException(string l, string f, string e)
+	{
+		err = (string)__DATE__ + ", " + (string)__TIME__ + ",Line: " + l + ", \nFile: " + f + ",\nError: " + e;
+	}
+
+	virtual const char* what() const noexcept
+	{
+		return err.c_str();
+	}
+
+	void saveError()
+	{
+		ofstream out("log.txt", ios::app);
+		out << err << endl;
+		out.close();
+	}
+};
---
+++ source.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+#include "List.h"
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    try
+    {
+        List<int> l;
+        l.push_front(2);
+        l.push_front(1);
+        l.push_front(12);
+        l.push_front(13);
+        l.push_front(14);
+
+        //Вийняток №1 - вихід за межі масиву
+        //cout << l[5] << '\n';
+
+        //Вийняток №2 - отримати доступ до першого чи останнього елементу списку
+        //при розмірі списку < 0 
+        l.clear();
+        cout << l.back() << '\n';
+
+
+        l.print();
+        l.print_reverse();
+
+
+    }
+
+    catch (exception& e)
+    {
+	cout << e.what() << endl;
+	MyException* ex = dynamic_cast<MyException*>(&e);
+	if(ex)
+		ex->saveError();
+    }
+
+  
+    std::cout << "Hello World!\n";
+
+    return 0;
+}
+
